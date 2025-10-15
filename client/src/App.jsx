a0e05a75b9bc1320d2b306c96277204936e1a9cd
Frankincense <frankincensewelsey@gmail.com>
2025-10-15 17:24:00 +0300

    Initialised files and folders, Initialised migrations for the database, initialised the frontend with files and routes

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router'
+import './App.css'
+
+
+function App() {
+    
+  return (
+    <>
+        <h1 clas>
+            How is this working
+        </h1>
+      <Outlet/> 
+    </>
+  )
+}
+
+export default App
---
+++ server/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Api
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from flask_cors import CORS
+from dotenv import load_dotenv
+from server.config import db
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app=app, supports_credentials=True)
+app.config.from_prefixed_env(prefix='FLASK')
+migrate = Migrate(app=app, db=db)
+bcrypt=Bcrypt(app=app)
+jwt = JWTManager(app=app)
+api = Api(app=app)
